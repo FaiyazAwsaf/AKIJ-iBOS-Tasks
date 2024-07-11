76d39f27f0e9d3ccd00653f4fc252ab40cc43803
Faiyaz A Awsaf <faiyazawsaf11@gmail.com>
2024-07-11 16:41:24 +0600

    Employee info page initial commit

---
+++ employee-list/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import EmployeeInfoPage from './components/EmployeeInfoPage';
+const App = () => {
+  return (
+    <div>
+      <EmployeeInfoPage />
+    </div>
+  );
+};
+
+export default App;
