f7587ef9a409d4576b2547fa52ee314145b1c88f
Faiyaz A Awsaf <faiyazawsaf11@gmail.com>
2024-07-17 20:36:32 +0600

    Update SalesReport.jsx

--- sales and payment report/src/components/SalesReport.jsx
+++ sales and payment report/src/components/SalesReport.jsx
@@ -5,26 +5,30 @@ const SalesReport = ({ fromDate, toDate }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get('https://apon.ibos.io/apon/wms/WmsReport/GetPOSSalesReport', {
-        headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJmZWYwNzVmMS00ZjEwLTQwNjUtYTFhZi1mMjlmZDkyZTE5MGMiLCJlbnJvbGwiOiJ5Z3IzZ3psd3U5ZDNFNFZmV2FHbW9BPT0iLCJlbWFpbGFkZHJlc3MiOiJhcmFmYXRAYXBvbi13ZWxsYmVpbmcuY29tIiwiYWNjb3VudCI6NSwidW5pdCI6MTg1LCJ1c2VyIjo1MjEyNjEsInN1YiI6ImFyYWZhdEBhcG9uLXdlbGxiZWluZy5jb20iLCJqdGkiOiJhNmQ0ODUyNC0xZDI1LTRkZDktYTkwMy03NmEzM2VjMTc2NmYiLCJpYXQiOiIwNy8wMi8yMDI0IDE0OjU5OjM1IiwiZXhwIjoxODE0NTQwMzc1LCJpc3MiOiJBa2lqSW5mb1RlY2ggTHRkLiAiLCJhdWQiOiJBdWRpZW5jZSJ9.Dikc2THMofamthFZ2_2qQvfL83dH_oMMc2Dyo-ta5fg',
+    try {
+      const fetchData = async () => {
+        const response = await axios.get('https://apon.ibos.io/apon/wms/WmsReport/GetPOSSalesReport', {
+          headers: {
+              'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJmZWYwNzVmMS00ZjEwLTQwNjUtYTFhZi1mMjlmZDkyZTE5MGMiLCJlbnJvbGwiOiJ5Z3IzZ3psd3U5ZDNFNFZmV2FHbW9BPT0iLCJlbWFpbGFkZHJlc3MiOiJhcmFmYXRAYXBvbi13ZWxsYmVpbmcuY29tIiwiYWNjb3VudCI6NSwidW5pdCI6MTg1LCJ1c2VyIjo1MjEyNjEsInN1YiI6ImFyYWZhdEBhcG9uLXdlbGxiZWluZy5jb20iLCJqdGkiOiJhNmQ0ODUyNC0xZDI1LTRkZDktYTkwMy03NmEzM2VjMTc2NmYiLCJpYXQiOiIwNy8wMi8yMDI0IDE0OjU5OjM1IiwiZXhwIjoxODE0NTQwMzc1LCJpc3MiOiJBa2lqSW5mb1RlY2ggTHRkLiAiLCJhdWQiOiJBdWRpZW5jZSJ9.Dikc2THMofamthFZ2_2qQvfL83dH_oMMc2Dyo-ta5fg',
+            },
+          params: {
+            reportTypeId: 1,
+            businessUnitId: 185,
+            warehouseId: 10177,
+            fromDate: fromDate,
+            toDate: toDate,
           },
-        params: {
-          reportTypeId: 1,
-          businessUnitId: 185,
-          warehouseId: 10177,
-          fromDate: fromDate,
-          toDate: toDate,
-        },
-      });
-      setData(response.data);
-      console.log(response.data)
+        });
+        setData(response.data);
+        console.log(response.data)
 
-    };
-
-    if (fromDate && toDate) {
-      fetchData();
+        if (fromDate && toDate) {
+          fetchData();
+        }
+      };
+    }
+    catch (error) {
+      console.error('Error creating employee', error);
     }
   }, [fromDate, toDate]);
 
